e315c1f8205a01b17ead93e04ff45a7cd3bd0169
AimerSanchez <aimersc09@hotmail.com>
2022-07-02 10:06:05 -0500

    GRAL: Se crea la estructura del proyecto

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyOperationImpl.java
@@ -0,0 +1,9 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+public class MyOperationImpl implements MyOperation{
+
+    @Override
+    public int suma(int number) {
+        return number+1;
+    }
+}
